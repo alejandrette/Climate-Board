7f562671c048ed1b5456e2f89aec42a1f958d8b4
Alejandrette <alejandrette5117@gmail.com>
2024-10-20 18:29:53 +0200

    create estructure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.css';
+import Home from "./components/Home.jsx";
+
+ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
