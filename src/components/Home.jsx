690c793725eadef8a5d4e6983f42150c2421899e
Alejandrette <alejandrette5117@gmail.com>
2024-10-24 13:08:25 +0200

    create backend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { API_KEY } from "./API_KEY";
+
+import Search from "./Search";
+import '../style/index.css';
+import Weather from "./Weather";
 
 const Home = () => {
+  /* const api = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${API_KEY}`;
+  
+  const api = `http://api.openweathermap.org/geo/1.0/direct?q=madrid&limit=1&appid=${API_KEY}`;
+  fetch(api)
+    .then((respo) => respo.json())
+    .then(data => {
+      console.log(data);
+    }) */
   return (
     <div>
+      <Search/>
+      <Weather/>
     </div>
   )
 }
